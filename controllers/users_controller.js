909c3392235c49897c6e9eccc17b3afdab9f51e0
Lorens <gueronlj@gmail.com>
2023-11-09 19:53:18 -0500

    succesfuly insert array of json objects as dates

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -10,15 +10,63 @@ const pool = new Pool({
   }
 });
 
+//fetch all habbits of user by user_id
 user.get('/:id/habits', async (req, res) => {
    try{
       const response = await pool.query(`
-      select * from habits where user_id = '${req.params.id}';`)
+      SELECT * FROM habits WHERE user_id = $1;`, [req.params.id]);
       res.json(response.rows);
    } catch(e){
-      console.error(e.message);
       res.send(`Error: ${e.message}`);
    }
 })
+
+//fetch a user by email
+user.get('/:email', async (req, res) => {
+   try{
+      const response = await pool.query(`
+      SELECT * FROM users WHERE email = $1;`, [req.params.email]);
+      res.json(response.rows);
+   } catch(e){
+      res.send(`Error: ${e.message}`);
+   }
+})
+
+//fetch user_id by email
+user.get('/:email/id', async (req, res) => {
+   try{
+      const response = await pool.query(`
+      SELECT id FROM users WHERE email = $1 LIMIT 1;`, [req.params.email]);
+      res.json(response.rows);
+   } catch(e){
+      res.send(`Error: ${e.message}`);
+   }
+})
+
+//create new user
+user.post('/', async (req, res) => {
+   try{
+      const response = await pool.query(`
+      insert into users (email) values ($1) returning *;`, [req.body.email]);
+      res.json(response.rows);
+   } catch(e){
+      res.send(`Error: ${e.message}`);
+   }
+})
+
+//insert new habit
+user.post('/habits', async (req, res) => {
+   try {
+      const response = await pool.query(`
+         INSERT INTO habits (title, dates, user_id) 
+         VALUES ($1, $2, $3) returning *;`, 
+         [req.body.title, req.body.dates, req.body.id]
+      );
+      res.send(response.rows);
+   } catch(e) {
+      res.send(`Error: ${e.message}`);
+   }
+})
+
 
 module.exports = user
