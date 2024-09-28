4604074d1d14bd871f94a30440bed4c5b5d29af0
Lorens <gueronlj@gmail.com>
2024-09-28 17:10:48 -0400

    Fixed: CORS options

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const app = express();
 
 const corsOptions ={
   origin:'http://localhost:5173', 
-  credentials:true,            //access-control-allow-credentials:true
   optionSuccessStatus:200
 }
 app.use(cors(corsOptions));
