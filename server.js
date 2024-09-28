d7f21023b5cb26ed544e4a35de0f7494fc7c9660
Lorens <gueronlj@gmail.com>
2024-09-28 19:59:30 -0400

    Removed: cors origin

--- server.js
+++ server.js
@@ -2,11 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express();
 
-const corsOptions ={
-  origin:'http://localhost:5173', 
-  optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 const userController = require(`./controllers/users_controller.js`)
