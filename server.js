d304c80fa5d7252fcdd3c4877dece0f14f37686e
Lorens <gueronlj@gmail.com>
2024-09-28 11:15:36 -0400

    Added: new CORS config

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ const express = require('express')
 const cors = require('cors')
 const app = express();
 
-app.use(cors({credentials: true}));
+const corsOptions ={
+  origin:'http://localhost:5173', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 app.use(express.json());
 
 const userController = require(`./controllers/users_controller.js`)
