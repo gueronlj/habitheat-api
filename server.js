c970c527aab3a9ff88e2fd2258a601c9a75108e1
Lorens <gueronlj@gmail.com>
2024-09-28 19:38:53 -0400

    more CORS

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 const userController = require(`./controllers/users_controller.js`)
-app.use(`/api/user`, userController)
+app.use(`/api/user`, cors(), userController)
 
 const habitController = require(`./controllers/habits_controller.js`)
-app.use(`/api/habits`, habitController)
+app.use(`/api/habits`, cors(), habitController)
 
 app.listen(3000, () => {
   console.log('Server started on port 3000');
